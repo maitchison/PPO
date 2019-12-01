Fixed to reward normalization

rl/models.py
@@ -49,7 +49,12 @@ class PolicyModel(nn.Module):
         x = x.to(self.device, non_blocking=True)
 
         # then covert the type (faster to upload uint8 then convert on GPU)
-        x = x.to(dtype=self.dtype, non_blocking=True)
+        if x.dtype == torch.uint8:
+            x = x.to(dtype=self.dtype, non_blocking=True) / 255
+        elif x.dtype == self.dtype:
+            x = x.to(dtype=self.dtype, non_blocking=True)
+        else:
+            raise Exception("Invalid dtype {}".format(x.dtype))
 
         return x
 
@@ -95,11 +100,11 @@ class CNNModel(PolicyModel):
             # make a batch of 1 for a single example.
             x = x[np.newaxis, :, :, :]
 
-        validate_dims(x, (None, *self.input_dims), np.uint8)
+        validate_dims(x, (None, *self.input_dims))
 
         n,c,w,h = x.shape
 
-        x = self.prep_for_model(x) / 255.0
+        x = self.prep_for_model(x)
 
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
@@ -139,77 +144,6 @@ class RNDModel(PolicyModel):
     def forward(self, x):
         return self.policy_model.forward(x)
 
-
-class ImprovedCNNModel(PolicyModel):
-    """ An improved CNN model that uses 3x3 filters and maxpool instead of strides.
-    """
-
-    name = "Improved_CNN"
-
-    def __init__(self, input_dims, actions, device, dtype, include_xy=True):
-
-        super(ImprovedCNNModel, self).__init__()
-
-        self.actions = actions
-        c, w, h = input_dims
-
-        self.include_xy = include_xy
-
-        if self.include_xy:
-            c = c + 2
-
-        self.conv1 = nn.Conv2d(c, 32, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-
-        w = get_CNN_output_size(w, [3, 3, 3, 3], [2, 2, 2, 1], max_pool=True)
-        h = get_CNN_output_size(h, [3, 3, 3, 3], [2, 2, 2, 1], max_pool=True)
-
-        self.out_shape = (64, w, h)
-
-        self.d = utils.prod(self.out_shape)
-        self.fc = nn.Linear(self.d, 512)
-        self.fc_policy = nn.Linear(512, actions)
-        self.fc_value = nn.Linear(512, 1)
-
-        self.set_device_and_dtype(device, dtype)
-
-    def forward(self, x):
-        """ forwards input through model, returns policy and value estimate. """
-
-        if len(x.shape) == 3:
-            # make a batch of 1 for a single example.
-            x = x[np.newaxis, :, :, :]
-
-        assert x.dtype == np.uint8, "invalid dtype for input, found {} expected {}.".format(x.dtype, "uint8")
-        assert len(x.shape) == 4, "input should be (N,C,W,H)"
-
-        n,c,w,h = x.shape
-
-        x = self.prep_for_model(x) / 255.0
-
-        # give filters access to x,y location
-        if self.include_xy:
-            x = add_xy(x)
-
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv4(x))
-
-        if x.shape[1:] != self.out_shape:
-            raise Exception("Invalid output dims. Expected {} found {}.".format(x.shape, self.out_shape))
-
-        x = F.relu(self.fc(x.view(n, self.d)))
-
-        policy = F.log_softmax(self.fc_policy(x), dim=1)
-        value = self.fc_value(x).squeeze(dim=1)
-
-        return policy, value
 
 class ICMModel(PolicyModel):
     """ Intrinsic curiosity model

rl/ppo.py
@@ -62,10 +62,21 @@ def train_minibatch(model: models.PolicyModel, optimizer, prev_states, next_stat
 
     # calculate RND gradient
     if args.use_rnd:
+
+        # note: this could be much much more efficent, I really should just be passing in mu and sigma, and then
+        # letting the model do the transformation... (i.e. keep everything as uint8)
+
         # train on only 25% of minibatch to slow down the predictor network when used with large number of agents.
+
         predictor_proportion = np.clip(32 / args.agents, 0.01, 1)
+
+        mu = atari.ENV_STATE["observation_norm_state"][0]
+        sigma = np.sqrt(atari.ENV_STATE["observation_norm_state"][1])
+
+        normed_states = np.asarray(np.clip((prev_states - mu) / (sigma + 0.0001), -5, 5), dtype=np.float32)
+
         n = int(len(prev_states) * predictor_proportion)
-        loss_rnd = model.prediction_error(prev_states[:n]).mean()
+        loss_rnd = model.prediction_error(normed_states[:n]).mean()
         loss += loss_rnd
         if my_counter % 100 == 0:
             print("loss_rnd {:.4f}".format(float(loss_rnd)))
@@ -183,8 +194,16 @@ def run_agents_vec(n_steps, model, vec_envs, states, episode_score, episode_len,
 
         # generate rnd bonus
         if args.use_rnd:
-            loss_rnd = model.prediction_error(prev_states).detach().cpu().numpy()
-            intrinsic_rewards += loss_rnd
+
+            # todo: norming should be handled by model... and I should update the model's constants..
+            if "observation_norm_state" in atari.ENV_STATE:
+                mu = atari.ENV_STATE["observation_norm_state"][0]
+                sigma = np.sqrt(atari.ENV_STATE["observation_norm_state"][1])
+
+                normed_states = np.asarray(np.clip((states - mu) / (sigma + 0.0001), -5, 5), dtype=np.float32)
+
+                loss_rnd = model.prediction_error(normed_states).detach().cpu().numpy()
+                intrinsic_rewards += loss_rnd
 
         raw_rewards = np.asarray([info.get("raw_reward", reward) for reward, info in zip(rewards, infos)], dtype=np.float32)
 
@@ -406,7 +425,7 @@ def train(env_name, model: models.PolicyModel):
         checkpoint_path = os.path.join(args.log_folder, checkpoints[0][1])
         restored_step, logs, norm_state = utils.load_checkpoint(checkpoint_path, model, optimizer)
 
-        for k, v in norm_state:
+        for k, v in norm_state.items():
             atari.ENV_STATE[k] = v
 
         training_log, timing_log, score_history, len_history = logs
@@ -502,6 +521,11 @@ def train(env_name, model: models.PolicyModel):
                 np.mean(batch_intrinsic_reward), np.std(batch_intrinsic_reward),
             ))
 
+        # stub:
+        # just add rewards together, should be processed separately with different gamma
+        batch_reward += batch_intrinsic_reward * 0.05
+
+        # ----------------------------------------------------
         # estimate advantages
 
         # we calculate the advantages by going backwards..

rl/wrappers.py
@@ -193,13 +193,14 @@ class NormalizeObservationsWrapper(gym.Wrapper):
     """
     Normalizes observations.
     """
-    def __init__(self, env, clip, initial_state=None):
+    def __init__(self, env, clip, save_to_info=False, initial_state=None):
         super().__init__(env)
 
         self.env = env
         self.epsilon = 1e-4
         self.clip = clip
         self.obs_rms = RunningMeanStd(shape=())
+        self.save_to_info=save_to_info
         if initial_state is not None:
             self.restore_state(initial_state)
 
@@ -211,12 +212,15 @@ class NormalizeObservationsWrapper(gym.Wrapper):
 
         scaled_obs = (obs - self.mean) / (self.std + self.epsilon)
         scaled_obs = np.clip(scaled_obs, -self.clip, +self.clip)
-        # stub: scale normalizaton to reasonable level
-        scaled_obs = (scaled_obs + 5) * 20
-        scaled_obs = np.asarray(scaled_obs, np.uint8)
+        scaled_obs = np.asarray(scaled_obs, dtype=np.float32)
 
         info["observation_norm_state"] = self.save_state()
-        return scaled_obs, reward, done, info
+
+        if self.save_to_info:
+            info["normalized_observation"] = scaled_obs
+            return obs, reward, done, info
+        else:
+            return scaled_obs, reward, done, info
 
     def save_state(self):
         """
@@ -291,7 +295,7 @@ class MonitorWrapper(gym.Wrapper):
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
         info["monitor_obs"] = obs.copy()
-        info["raw_rewards"] = reward
+        info["raw_reward"] = reward
         return obs, reward, done, info
 
 class FrameCropWrapper(gym.Wrapper):