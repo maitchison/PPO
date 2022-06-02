1b77b8ef06f551d58a01e19dda2506f2a46622c2
Matthew Aitchison <matthew.aitchison@anu.edu.au>
2022-06-02 20:50:47 +1000

    changes

--- runner_tools.py
+++ runner_tools.py
@@ -629,7 +629,9 @@ class Job:
         params["output_folder"] = "./Run"
         params["experiment_name"] = self.experiment_name
         params["run_name"] = self.run_name
+        params['restore'] = True
         params['error_on_missing_restore'] = False # restore if we can, but do not error if we can not.
+
         nice_params = [
             f"--{k}={nice_format(v)}" for k, v in params.items() if k not in ["env_name"] and v is not None
         ]
